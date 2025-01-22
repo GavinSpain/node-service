1dd9bf8a2ea2f52fa4fe268e7f27917633d2eaa5
GS <155876934+GavinSpain@users.noreply.github.com>
2025-01-22 22:54:25 +1100

    Allow CORS

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,21 @@
 function setupRoutes(app) {
     app.get('/api/heartbeatstatus', (req, res) => {
+        // Enable CORS
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET');
+        
         const heartbeatData = [];
         
         for (let i = 1; i <= 5; i++) {
             heartbeatData.push({
                 service_id: i,
-                seconds_since_last: Math.floor(Math.random() * 40)
+
+                seconds_since_last: Math.floor(Math.random() * 30)
             });
         }
         
         res.json(heartbeatData);
     });
 }
+
 module.exports = setupRoutes;
