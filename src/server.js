b536a68c814d1a8b6579afa9eb51fcd2c8ce5ef1
GS <155876934+GavinSpain@users.noreply.github.com>
2025-01-22 23:48:13 +1100

    update SSL certificate paths to include 'www' subdomain

--- src/server.js
+++ src/server.js
@@ -3,13 +3,13 @@ const fs = require('fs');
 const app = require('./app');
 
 const options = {
-    key: fs.readFileSync('/etc/letsencrypt/live/spainymatrix.xyz/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/spainymatrix.xyz/fullchain.pem')
+    key: fs.readFileSync('/etc/letsencrypt/live/www.spainymatrix.xyz/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/www.spainymatrix.xyz/fullchain.pem')
 };
 
 const server = https.createServer(options, app);
 
 server.listen(3443, () => {
     console.log('HTTPS Server started on port 3443');
-    console.log('Using SSL certificate for spainymatrix.xyz');
+    console.log('Using SSL certificate for www.spainymatrix.xyz');
 });
